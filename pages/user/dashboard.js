42750222fb4d7fcfbe208e331a879d2b311656d2
Oscar Voit <oscarvoit@gmail.com>
2022-01-24 15:41:08 -0300

    atualizando user dashboard

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,10 +1,22 @@
-import { Button, Container, Typography } from '@material-ui/core'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import TemplateDefault from '../../src/templates/Default'
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: theme.spacing(8,0,6)
+  },
+  cardMedia: {
+    paddingTop: '56%',
   },
   buttonAdd: {
     margin: '30px auto',
@@ -21,7 +33,98 @@ export default function Home() {
           Meus Anúncios
         </Typography>
         <Button variant="contained" color="primary" className={classes.buttonAdd}>Publicar novo anúncio</Button>
+      </Container>
+      <Container maxWidth="md">
+        <Grid container spacing="4">
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">Editar</Button>
+                <Button size="small" color="primary">Remover</Button>
+              </CardActions>
+            </Card>
+          </Grid>
 
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">Editar</Button>
+                <Button size="small" color="primary">Remover</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">Editar</Button>
+                <Button size="small" color="primary">Remover</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto
+                </Typography>
+                <Typography>
+                  R$ 60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">Editar</Button>
+                <Button size="small" color="primary">Remover</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          
+        </Grid>
       </Container>
     </TemplateDefault>
   )
